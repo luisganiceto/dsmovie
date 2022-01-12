da59bb480d6cfcca32fb1061d4f84255588e0176
Luis Aniceto <luisg.aniceto@hotmail.com>
2022-01-12 13:34:26 -0300

    MovieCard Layout

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,7 +1,32 @@
 import { Pagination } from "components/Pagination";
+import { MovieCard } from "components/MovieCard";
 
-export function Listing(){
-    return(
-        <Pagination />
+export function Listing() {
+    return (
+        <>
+            <Pagination />
+            <div className="container">
+                <div className="row">
+                    <div className="col-xl-4 col-md-6 col-sm-12 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-xl-4 col-md-6 col-sm-12 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-xl-4 col-md-6 col-sm-12 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-xl-4 col-md-6 col-sm-12 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-xl-4 col-md-6 col-sm-12 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-xl-4 col-md-6 col-sm-12 mb-3">
+                        <MovieCard />
+                    </div>
+                </div>
+            </div>
+        </>
     );
 }
